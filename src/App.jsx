3ac6c0f8d0078d859d830a32470fd3ac1fbdb7d9
Shivang <kshivang679@gmail.com>
2025-08-31 18:02:29 +0530

    Initial commit : Meme generator project made

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./components/Header"
+import Main from "./Main"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
